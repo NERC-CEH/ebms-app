6a08eb0f7ad02546b2ee55cd9ccae6e4def9157a
kazlauskis <karolis@kazlauskis.com>
2019-06-13 13:36:24 +0100

    Fix iOS mapping visuals

--- src/vendor.js
+++ src/vendor.js
@@ -13,3 +13,6 @@ import 'react';
 import 'react-dom';
 import 'indicia';
 import 'bigu';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import 'leaflet-draw/dist/images/spritesheet.svg';
