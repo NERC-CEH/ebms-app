5c4b7cace50abfb5a438edc3d745c9da61d1a7fc
kazlauskis <karolis@kazlauskis.com>
2019-06-14 12:35:21 +0100

    Refactor settings page

--- src/Settings/Menu/Main.js
+++ src/Settings/Menu/Main.js
@@ -4,86 +4,7 @@ import PropTypes from 'prop-types';
 import Toggle from 'common/Components/Toggle';
 import { IonContent, IonPopover, IonIcon } from '@ionic/react';
 import alert from 'common/helpers/alert';
-import Log from 'helpers/log';
 
-// function deleteAllSamples(savedSamples) {
-//   const body = `${t(
-//     'Are you sure you want to remove all successfully synchronised local records?'
-//   )}<p><i><b>${t('Note')}:</b> ${t(
-//     'records on the server will not be touched.'
-//   )}</i></p>`;
-
-//   radio.trigger('app:dialog', {
-//     title: 'Remove All',
-//     body,
-//     buttons: [
-//       {
-//         title: 'Cancel',
-//         fill: 'clear',
-//         onClick() {
-//           radio.trigger('app:dialog:hide');
-//         },
-//       },
-//       {
-//         title: 'Remove',
-//         color: 'danger',
-//         onClick() {
-//           Log('Settings:Menu:Controller: deleting all samples.');
-
-//           // delete all
-//           savedSamples
-//             .removeAllSynced()
-//             .then(() => {
-//               radio.trigger('app:dialog', {
-//                 title: 'Done!',
-//                 timeout: 1000,
-//               });
-//             })
-//             .catch(err => {
-//               Log(err, 'e');
-//               radio.trigger('app:dialog:error', err);
-//             });
-//           Analytics.trackEvent('Settings', 'delete all');
-//         },
-//       },
-//     ],
-//   });
-// }
-
-// function sendAllSamples(savedSamples) {
-//   radio.trigger('app:dialog', {
-//     title: 'Submit All',
-//     body: 'Are you sure you want to set all valid records for submission?',
-//     buttons: [
-//       {
-//         title: 'Cancel',
-//         fill: 'clear',
-//         onClick() {
-//           radio.trigger('app:dialog:hide');
-//         },
-//       },
-//       {
-//         title: 'Submit',
-//         onClick() {
-//           Log('Settings:Menu:Controller: sending all samples.');
-//           savedSamples
-//             .setAllToSend()
-//             .then(() => {
-//               radio.trigger('app:dialog', {
-//                 title: 'Done!',
-//                 timeout: 1000,
-//               });
-//             })
-//             .catch(err => {
-//               Log(err, 'e');
-//               radio.trigger('app:dialog:error', err);
-//             });
-//           Analytics.trackEvent('Settings', 'send all');
-//         },
-//       },
-//     ],
-//   });
-// }
 
 function resetDialog(resetApp) {
   alert({
@@ -108,22 +29,20 @@ function resetDialog(resetApp) {
 
 @observer
 class Component extends React.Component {
-  state = { showPopover: false };
-
-  onToggle = (setting, checked) => {
-    const { appModel } = this.props;
-    Log('Settings:Menu:Controller: setting toggled.');
-    appModel.set(setting, checked);
-    appModel.save();
+  static propTypes = {
+    resetApp: PropTypes.func.isRequired,
+    onToggle: PropTypes.func.isRequired,
+    useTraining: PropTypes.bool.isRequired,
   };
+
+  state = { showPopover: false };
 
   setShowPopover = val => {
     this.setState({ showPopover: val });
   };
 
   render() {
-    const { resetApp, appModel } = this.props;
-    const useTraining = appModel.get('useTraining');
+    const { resetApp, onToggle, useTraining } = this.props;
 
     const ToDoPopover = (
       <IonPopover
@@ -166,7 +85,7 @@ class Component extends React.Component {
               <IonIcon name="school" size="small" slot="start" />
               <ion-label>{t('Training Mode')}</ion-label>
               <Toggle
-                onToggle={checked => this.onToggle('useTraining', checked)}
+                onToggle={checked => onToggle('useTraining', checked)}
                 checked={useTraining}
               />
             </ion-item>
@@ -180,10 +99,5 @@ class Component extends React.Component {
     );
   }
 }
-
-Component.propTypes = {
-  resetApp: PropTypes.func.isRequired,
-  appModel: PropTypes.object.isRequired,
-};
 
 export default Component;
