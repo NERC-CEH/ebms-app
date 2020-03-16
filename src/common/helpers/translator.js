dbcd6658ea69fe7c47597f8eb3e28c224eb658e4
Karolis Kazlauskis <karolis@flumens.io>
2020-03-16 11:36:46 +0000

    Disable French, Hungarian and French

--- src/common/helpers/translator.js
+++ src/common/helpers/translator.js
@@ -100,14 +100,15 @@ const names = {
 };
 
 export const languages = {
+  // disabled - under-translated (< 90%)
   en: 'English',
   lt_LT: 'Lietuvių',
   es_ES: 'Español',
   sv_SE: 'Svenska',
-  hr_HR: 'Hrvatski',
+  // hr_HR: 'Hrvatski',
   nl_NL: 'Nederlands',
-  fi_FI: 'Suomi',
-  fr_FR: 'Français',
+  // fi_FI: 'Suomi',
+  // fr_FR: 'Français',
   ru_RU: 'Pусский',
   pt_PT: 'Português',
   de_DE: 'Deutsch',
