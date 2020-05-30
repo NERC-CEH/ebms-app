73ae135c870ed0ff568cb0719559939e188023ab
Vilius Stankaitis <stankaitis@Viliuss-MBP.connect>
2020-05-30 14:22:27 +0100

    Add attribute page lib component

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,7 @@ const config = {
       path.resolve(ROOT_DIR, './src/common/vendor'),
     ],
     alias: {
+      '@apps': 'common/appsBitCollection',
       Lib: 'common/Lib',
       Components: 'common/Components',
       config: 'common/config/config',
@@ -153,16 +154,24 @@ const config = {
 
       'process.env': {
         // package.json variables
-        APP_BUILD: JSON.stringify(process.env.TRAVIS_BUILD_ID || pkg.build || new Date().getTime()),
+        APP_BUILD: JSON.stringify(
+          process.env.TRAVIS_BUILD_ID || pkg.build || new Date().getTime()
+        ),
         APP_NAME: JSON.stringify(pkg.name), // no need to be an env value
         APP_VERSION: JSON.stringify(pkg.version), // no need to be an env value
 
         // mandatory env. variables
-        APP_INDICIA_API_KEY: JSON.stringify(process.env.APP_INDICIA_API_KEY || ''),
-        APP_MAPBOX_MAP_KEY: JSON.stringify(process.env.APP_MAPBOX_MAP_KEY || ''),
+        APP_INDICIA_API_KEY: JSON.stringify(
+          process.env.APP_INDICIA_API_KEY || ''
+        ),
+        APP_MAPBOX_MAP_KEY: JSON.stringify(
+          process.env.APP_MAPBOX_MAP_KEY || ''
+        ),
 
         // compulsory env. variables
-        APP_INDICIA_API_HOST: JSON.stringify(process.env.APP_INDICIA_API_HOST || ''),
+        APP_INDICIA_API_HOST: JSON.stringify(
+          process.env.APP_INDICIA_API_HOST || ''
+        ),
         APP_TRAINING: process.env.APP_TRAINING || false,
         APP_EXPERIMENTS: process.env.APP_EXPERIMENTS || false,
         APP_SENTRY_KEY: JSON.stringify(process.env.APP_SENTRY_KEY || ''),
