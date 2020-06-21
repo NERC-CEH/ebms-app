8c86bcdf3878b482358b4dca8aba401c7496ac57
Vilius Stankaitis <vilius@stankaitis.com>
2020-06-21 09:43:36 +0100

    Fix training mode style and translation

--- src/Survey/Transect/Edit/Header/index.js
+++ src/Survey/Transect/Edit/Header/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { IonButton } from '@ionic/react';
-import Header from 'Lib/Header';
+import Header from '@bit/flumens.apps.header';
+import { Trans as T } from 'react-i18next';
 import './styles.scss';
 
 function getFinishButton(onSubmit, isEditing) {
-  const label = isEditing ? t('Upload') : t('Finish');
+  const label = isEditing ? <T>Upload</T> : <T>Finish</T>;
   return <IonButton onClick={onSubmit}>{label}</IonButton>;
 }
 
 const HeaderComponent = ({ onSubmit, isTraining, isEditing, isDisabled }) => {
   const trainingModeSubheader = isTraining && (
-    <div className="training-survey">training survey</div>
+    <div className="training-survey">
+      <T>Training Mode</T>
+    </div>
   );
 
   return (
     <Header
-      title={t('Transect')}
+      title="Transect"
       rightSlot={!isDisabled && getFinishButton(onSubmit, isEditing)}
       subheader={trainingModeSubheader}
       defaultHref="/home/user-surveys"
