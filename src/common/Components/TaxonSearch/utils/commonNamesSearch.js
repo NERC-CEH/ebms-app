a9ac8f293bb840aaef7d3be4c7203df11aef0e69
Vilius Stankaitis <vilius@stankaitis.com>
2020-07-06 14:41:19 +0100

    Fix taxon sorting by common names

--- src/common/Components/TaxonSearch/utils/commonNamesSearch.js
+++ src/common/Components/TaxonSearch/utils/commonNamesSearch.js
@@ -1,7 +1,9 @@
 import appModel from 'app_model';
 import speciesNames from 'common/data/commonNames';
 
-export default (normSearchPhrase, results, maxResults) => {
+const MAX_RESULTS = 200;
+
+export default (normSearchPhrase, results) => {
   const { language } = appModel.attrs;
   const languageSpeciesNames = speciesNames[language];
   if (!languageSpeciesNames) {
@@ -13,7 +15,7 @@ export default (normSearchPhrase, results, maxResults) => {
     // eslint-disable-next-line
     ({ warehouse_id, scientific_name, common_name, preferredId }) => {
       const matches = common_name.match(new RegExp(normSearchPhrase, 'i'));
-      if (matches && results.length + commonNames.length <= maxResults) {
+      if (matches && results.length + commonNames.length <= MAX_RESULTS) {
         commonNames.push({
           found_in_name: 'common_name',
           warehouse_id,
@@ -25,9 +27,12 @@ export default (normSearchPhrase, results, maxResults) => {
     }
   );
 
-  commonNames = commonNames.sort((sp1, sp2) =>
-    sp1.common_name.localeCompare(sp2.common_name)
-  );
+  commonNames = commonNames
+    .sort((sp1, sp2) => sp1.common_name.localeCompare(sp2.common_name))
+    .sort(
+      (sp1, sp2) =>
+        sp1.common_name.split(' ').length - sp2.common_name.split(' ').length
+    );
 
   results.push(...commonNames);
   return results;
