2f576a9d4b28201bbee8e9cdbbee4ed7db42fca3
Karolis Kazlauskis <karolis@flumens.io>
2020-10-22 09:46:37 +0100

    Re-enable env check

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,15 @@
 require('dotenv').config({ silent: true }); // get local environment variables from .env
 const checkEnv = require('@flumens/has-env');
 
-// checkEnv({
-//   warn: ['APP_TRAINING', 'APP_MANUAL_TESTING', 'APP_INDICIA_API_HOST'],
-//   required: [
-//     'APP_MAPBOX_MAP_KEY',
-//     'APP_SENTRY_KEY',
-//     'APP_INDICIA_API_KEY',
-//     'APP_WEATHER_SITE_API_KEY',
-//   ],
-// });
+checkEnv({
+  warn: ['APP_TRAINING', 'APP_MANUAL_TESTING', 'APP_INDICIA_API_HOST'],
+  required: [
+    'APP_MAPBOX_MAP_KEY',
+    'APP_SENTRY_KEY',
+    'APP_INDICIA_API_KEY',
+    'APP_WEATHER_SITE_API_KEY',
+  ],
+});
 
 const path = require('path');
 const webpack = require('webpack');
