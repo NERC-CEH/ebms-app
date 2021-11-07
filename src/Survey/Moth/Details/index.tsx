64650a7607aac44824957ab00578cf312c004378
Vilius Stankaitis <vilius@stankaitis.com>
2021-11-07 10:38:01 +0200

    Add details page complete details func

--- src/Survey/Moth/Details/index.tsx
+++ src/Survey/Moth/Details/index.tsx
@@ -1,7 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import Sample from 'models/sample';
 import { observer } from 'mobx-react';
-import { Page, Header } from '@apps';
+import { Page, Header, showInvalidsMessage } from '@apps';
+import { IonButton, NavContext } from '@ionic/react';
+import { Trans as T } from 'react-i18next';
+import { useRouteMatch } from 'react-router';
 import Main from './Main';
 
 type Props = {
@@ -9,9 +12,33 @@ type Props = {
 };
 
 const DetailsController: FC<Props> = ({ sample }) => {
+  const { navigate } = useContext(NavContext);
+  const match = useRouteMatch();
+
+  const onFinish = () => {
+    const invalids = sample.validateRemote();
+
+    if (invalids) {
+      showInvalidsMessage(invalids);
+      return;
+    }
+
+    sample.metadata.completedDetails = true; // eslint-disable-line
+    sample.save();
+
+    const url = match.url.replace('/edit', '');
+    navigate(url, 'none', 'replace');
+  };
+
+  const getNextButton = sample.metadata.completedDetails ? null : (
+    <IonButton onClick={onFinish}>
+      <T>Next</T>
+    </IonButton>
+  );
+
   return (
     <Page id="survey-moth-detail">
-      <Header title="Survey Details" />
+      <Header title="Survey Details" rightSlot={getNextButton} />
       <Main sample={sample} />
     </Page>
   );
