bb6ccf868c8fd4eb7b4c5a68a985d3517a15e5a5
Vilius Stankaitis <vilius@stankaitis.com>
2021-11-10 11:16:55 +0200

    Add Finish func to moth survey

--- src/Survey/Moth/Home/index.tsx
+++ src/Survey/Moth/Home/index.tsx
@@ -1,19 +1,23 @@
 /* eslint-disable camelcase */
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import Sample from 'models/sample';
+import appModel from 'models/appModel';
 import { observer } from 'mobx-react';
 import { Plugins, HapticsImpactStyle } from '@capacitor/core';
 import { useRouteMatch } from 'react-router';
-import { Page, Header, alert } from '@apps';
-import { isPlatform } from '@ionic/react';
+import { Page, Header, alert, showInvalidsMessage } from '@apps';
+import { isPlatform, IonButton, NavContext } from '@ionic/react';
+import { Trans as T } from 'react-i18next';
 import Occurrence from 'models/occurrence';
 import Main from './Main';
 import './styles.scss';
 
 const { Haptics } = Plugins;
 
-function deleteOccurrence(occ: typeof Occurrence) {
-  const { scientific_name } = occ.attrs.taxon;
+function setSurveyEndTime(sample: typeof Sample) {
+  sample.attrs.surveyEndime = new Date(); // eslint-disable-line no-param-reassign
+  return sample.save();
+}
 
   alert({
     header: 'Delete',
@@ -40,8 +44,47 @@ interface Props {
 }
 
 const HomeController: FC<Props> = ({ sample }) => {
+  const { navigate } = useContext(NavContext);
   const match = useRouteMatch();
   const isDisabled = sample.isDisabled();
+  const isEditing = sample.metadata.saved;
+
+  const _processSubmission = async () => {
+    if (await sample.upload()) navigate(`/home/user-surveys`, 'root');
+  };
+
+  const _processDraft = async () => {
+    const surveyName = sample.getSurvey().name;
+    appModel.attrs[`draftId:${surveyName}`] = null;
+    await appModel.save();
+
+    const saveAndReturn = () => {
+      setSurveyEndTime(sample);
+
+      sample.cleanUp();
+      sample.save();
+      navigate(`/home/user-surveys`, 'root');
+    };
+
+    const invalids = sample.validateRemote();
+    if (invalids) {
+      showInvalidsMessage(invalids, saveAndReturn);
+      return;
+    }
+
+    // eslint-disable-next-line no-param-reassign
+    sample.metadata.saved = true;
+    saveAndReturn();
+  };
+
+  const onSubmit = async () => {
+    if (!sample.metadata.saved) {
+      await _processDraft();
+      return;
+    }
+
+    await _processSubmission();
+  };
 
   const increaseCount = (taxon: any) => {
     if (sample.isDisabled()) {
@@ -57,9 +100,17 @@ const HomeController: FC<Props> = ({ sample }) => {
     isPlatform('hybrid') && Haptics.impact({ style: HapticsImpactStyle.Heavy });
   };
 
+  const getFinishButton = () => {
+    const label = isEditing ? <T>Upload</T> : <T>Finish</T>;
+    return <IonButton onClick={onSubmit}>{label}</IonButton>;
+  };
+
   return (
     <Page id="survey-moth-home">
-      <Header title="Moth-trap survey" />
+      <Header
+        title="Moth-trap survey"
+        rightSlot={!isDisabled && getFinishButton()}
+      />
       <Main
         match={match}
         sample={sample}
