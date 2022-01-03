ffb304adc464ce90f5eaff42a0360ef7e90592c5
Vilius Stankaitis <vilius@stankaitis.com>
2022-01-04 00:16:00 +0200

    Match taxon common name to specific language

--- src/common/models/occurrence.js
+++ src/common/models/occurrence.js
@@ -1,5 +1,6 @@
 import { Occurrence } from '@apps';
 import appModel from 'appModel';
+import speciesCommonNamesData from 'common/data/commonNames/index.json';
 import Media from './media';
 
 export default class AppOccurrence extends Occurrence {
@@ -47,13 +48,31 @@ export default class AppOccurrence extends Occurrence {
     };
     species.forEach(findHighestProbSpecies);
 
+    const { language } = appModel.attrs;
+    const speciesDataBySpecificLanguage = speciesCommonNamesData[language];
+
+    const byTaxonWithHighestProbability = spName =>
+      spName.preferredId ===
+      parseInt(highestProbSpecies?.taxa_taxon_list_id, 10);
+
+    const taxon = speciesDataBySpecificLanguage.find(
+      byTaxonWithHighestProbability
+    );
+
     if (!highestProbSpecies) return this.attrs.taxon;
 
-    this.attrs.taxon = {
-      warehouse_id: parseInt(highestProbSpecies.taxa_taxon_list_id, 10),
-      scientific_name: highestProbSpecies.taxon,
-      found_in_name: 'scientific_name',
-    };
+    if (taxon) {
+      this.attrs.taxon = {
+        ...taxon,
+        found_in_name: 'common_name',
+      };
+    } else {
+      this.attrs.taxon = {
+        warehouse_id: parseInt(highestProbSpecies.taxa_taxon_list_id, 10),
+        scientific_name: highestProbSpecies.taxon,
+        found_in_name: 'scientific_name',
+      };
+    }
 
     this.save();
 
