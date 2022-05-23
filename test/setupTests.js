fd9937c45365ec244f99b165cc95e8735f9c2583
Karolis Kazlauskis <karolis@flumens.io>
2022-05-23 17:03:42 +0300

    Fix Jest tests

---
+++ test/setupTests.js
@@ -0,0 +1,43 @@
+import 'regenerator-runtime/runtime';
+
+const mock = query => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+});
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(mock),
+});
+
+Object.defineProperty(window, 'L', {
+  writable: true,
+  value: {
+    Polyline: { extend() {} },
+  },
+});
+
+const localStorageMock = (function () {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+  };
+})();
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
