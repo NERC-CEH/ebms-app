7cfc24cf2a3f685ec77c24f0a4aa3dc787897a52
Karolis Kazlauskis <karolis@flumens.io>
2022-05-27 15:24:20 +0300

    Fix animation

--- src/Survey/common/IncrementalButton/AnimatedNumber/index.tsx
+++ src/Survey/common/IncrementalButton/AnimatedNumber/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react';
+import { FC, useState, useEffect, useRef } from 'react';
 import { CreateAnimation } from '@ionic/react';
 import './styles.scss';
 
@@ -8,25 +8,29 @@ type Props = {
 
 const AnimatedNumber: FC<Props> = ({ value }: any) => {
   const [initialised, setInitialised] = useState<boolean>(false);
-  const [play, setPlay] = useState<boolean>();
+  const first = useRef<any>();
 
   const playAnimation = () => {
-    initialised && setPlay(true);
-    setInitialised(true); // not on the first load
+    if (!initialised) {
+      setInitialised(true); // not on the first load
+      return;
+    }
+
+    // doing this programmatically to reset the progress
+    // on value change before animation finishes
+    first.current.animation.progressStep(0);
+    first.current.animation.play();
   };
-  useEffect(playAnimation, [value]);
+  useEffect(playAnimation, [value, first]);
 
   return (
     <CreateAnimation
       duration={2500}
+      ref={first}
       fromTo={[
         { property: 'color', fromValue: 'green', toValue: '' },
         { property: 'background', fromValue: '#00800024', toValue: '' },
       ]}
-      onFinish={{
-        callback: () => setPlay(false),
-      }}
-      play={play}
     >
       <span className="animated-number">{value}</span>
     </CreateAnimation>
