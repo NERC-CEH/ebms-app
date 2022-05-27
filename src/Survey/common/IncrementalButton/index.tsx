94debd09f4cf407fc0a7010ea816ba3910402dcc
Karolis Kazlauskis <karolis@flumens.io>
2022-05-27 15:26:15 +0300

    Add long-tap to incremental btn

--- src/Survey/common/IncrementalButton/index.tsx
+++ src/Survey/common/IncrementalButton/index.tsx
@@ -1,36 +1,49 @@
 import { FC } from 'react';
-import { isPlatform, IonButton } from '@ionic/react';
+import { isPlatform } from '@ionic/react';
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
+import { LongPressButton } from '@flumens';
 import AnimatedNumber from './AnimatedNumber';
 import './styles.scss';
 
 interface Props {
   onClick: any;
+  onLongClick?: any;
   value: number;
   disabled?: boolean;
 }
 
 const IncrementalButton: FC<Props> = ({
   onClick: onClickProp,
+  onLongClick: onLongClickProp,
   value,
   disabled,
 }) => {
-  const onClick = (e: any) => {
-    e.preventDefault();
-    e.stopPropagation();
-
+  const onClick = () => {
     if (disabled) return;
 
     onClickProp();
+
+    isPlatform('hybrid') && Haptics.impact({ style: ImpactStyle.Medium });
+  };
+
+  const onLongClick = () => {
+    if (disabled) return;
+
+    onLongClickProp();
 
     isPlatform('hybrid') && Haptics.impact({ style: ImpactStyle.Heavy });
   };
 
   return (
-    <IonButton className="incremental-button" onClick={onClick} fill="clear">
+    <LongPressButton
+      className="incremental-button"
+      onClick={onClick}
+      fill="clear"
+      onLongClick={onLongClick}
+    >
       <AnimatedNumber value={value} />
       <div className="label-divider" />
-    </IonButton>
+    </LongPressButton>
   );
 };
 
