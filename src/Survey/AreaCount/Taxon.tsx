0bc6bbd8ce8af9d9a707747166c9485124dbdd90
Vilius Stankaitis <vilius@stankaitis.com>
2022-06-14 10:37:29 +0300

    *588 Fix: Merge alert dont exist in the 15min count survey

---
+++ src/Survey/AreaCount/Taxon.tsx
@@ -0,0 +1,132 @@
+import { FC, useContext } from 'react';
+import Sample from 'models/sample';
+import appModel from 'models/app';
+import Occurrence from 'models/occurrence';
+import { observer } from 'mobx-react';
+import TaxonSearch from 'Components/TaxonSearch';
+import { NavContext } from '@ionic/react';
+import { useRouteMatch } from 'react-router';
+import { Page, Main, Header, useAlert } from '@flumens';
+import { Trans as T } from 'react-i18next';
+
+async function showMergeSpeciesAlert(alert: any) {
+  const showMergeSpeciesDialog = (resolve: any) => {
+    alert({
+      header: 'Species already exists',
+      message: (
+        <T>
+          Are you sure you want to merge this list to the existing species list?
+        </T>
+      ),
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            resolve(false);
+          },
+        },
+        {
+          text: 'Merge',
+          cssClass: 'primary',
+          handler: () => {
+            resolve(true);
+          },
+        },
+      ],
+    });
+  };
+  return new Promise(showMergeSpeciesDialog);
+}
+
+type Props = {
+  sample: Sample;
+  occurrence: Occurrence;
+};
+
+const TaxonController: FC<Props> = ({ sample, occurrence }) => {
+  const { goBack } = useContext(NavContext);
+  const match = useRouteMatch();
+  const alert = useAlert();
+
+  const onSpeciesSelected = async (taxon: any) => {
+    const { taxa }: any = match.params;
+    const { isRecorded } = taxon;
+
+    if (taxa && isRecorded) {
+      const mergeSpecies = await showMergeSpeciesAlert(alert);
+
+      if (!mergeSpecies) {
+        return;
+      }
+    }
+
+    if (taxa) {
+      const selectedTaxon = ({ occurrences }: Sample) => {
+        const [occ] = occurrences; // always one
+        return occ.attrs.taxon.warehouse_id === parseInt(taxa, 10);
+      };
+      const assignTaxon = ({ occurrences }: Sample) => {
+        const [occ] = occurrences; // always one
+        occ.attrs.taxon = taxon;
+      };
+      sample.samples.filter(selectedTaxon).forEach(assignTaxon);
+
+      await sample.save();
+
+      goBack();
+
+      return;
+    }
+
+    if (occurrence) {
+      // eslint-disable-next-line
+      occurrence.attrs.taxon = taxon;
+    } else {
+      const survey = sample.getSurvey();
+      const zeroAbundance = sample.isSurveyPreciseSingleSpecies() ? 't' : null;
+
+      const newSample = survey.smp.create(
+        Sample,
+        Occurrence,
+        taxon,
+        zeroAbundance
+      );
+      sample.samples.push(newSample);
+
+      if (!sample.isSurveyPreciseSingleSpecies()) {
+        newSample.startGPS();
+      }
+    }
+
+    await sample.save();
+    goBack();
+  };
+
+  const getTaxonId = (smp: Sample) => {
+    const occ = smp.occurrences[0];
+    return occ.attrs.taxon.preferredId || occ.attrs.taxon.warehouse_id;
+  };
+  const species = sample.samples.map(getTaxonId);
+
+  const getShallowTaxonId = (taxon: any) =>
+    taxon.preferredId || taxon.warehouse_id;
+  const shallowSpecies = sample.shallowSpeciesList.map(getShallowTaxonId);
+
+  const recordedTaxa = [...species, ...shallowSpecies];
+
+  return (
+    <Page id="precise-area-count-edit-taxa">
+      <Header title="Species" />
+      <Main>
+        <TaxonSearch
+          onSpeciesSelected={onSpeciesSelected}
+          recordedTaxa={recordedTaxa}
+          speciesGroups={appModel.attrs.speciesGroups}
+        />
+      </Main>
+    </Page>
+  );
+};
+
+export default observer(TaxonController);
