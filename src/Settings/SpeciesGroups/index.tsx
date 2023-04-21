554fac61051dc05c4deef493ef85e8d462781f2d
vilius-stankaitis <vilius@stankaitis.com>
2023-04-21 13:14:08 +0300

    Remove image

--- src/Settings/SpeciesGroups/index.tsx
+++ src/Settings/SpeciesGroups/index.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
 import { Page, Header, Main, InfoMessage } from '@flumens';
-import { IonList } from '@ionic/react';
 import { informationCircleOutline } from 'ionicons/icons';
 import { Trans as T } from 'react-i18next';
-import explainImg from './image.png';
 import './styles.scss';
 
 // TODO: DEPRECATED
@@ -15,12 +13,6 @@ const SpeciesGroups: FC = () => {
         <InfoMessage className="blue" icon={informationCircleOutline}>
           <T>This option was moved to the species search page.</T>
         </InfoMessage>
-
-        <IonList>
-          <div className="image">
-            <img src={explainImg} />
-          </div>
-        </IonList>
       </Main>
     </Page>
   );
