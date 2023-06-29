69bf9e2ad9796ae1d1f2daba328eb0178872eb17
Karolis Kazlauskis <karolis@kazlauskis.com>
2023-06-29 11:25:14 +0300

    Add a temp fix to recover from too many thumbnails

--- src/common/models/store.js
+++ src/common/models/store.js
@@ -11,6 +11,45 @@ export const modelStore = new Store({
   storeName: 'models',
   debugging: isDemo,
 });
+
+modelStore.findAll = async () => {
+  console.log('Fixing old pics');
+
+  const db = window.sqlitePlugin.openDatabase({
+    name: 'indicia',
+    location: 'default',
+  });
+
+  const execute = query =>
+    new Promise((resolve, reject) => {
+      db.transaction(t => {
+        t.executeSql(
+          query,
+          [],
+          (_, results) => {
+            const rows = [];
+            console.log('rowsAffected', results.rowsAffected);
+            for (let i = 0; i < results.rows.length; i++) {
+              rows.push(results.rows.item(i));
+            }
+            resolve(rows);
+          },
+          (_, error) => {
+            reject(error);
+          }
+        );
+      });
+    });
+
+  await execute(
+    `DELETE FROM models as m WHERE json_extract(m.value, '$.metadata.syncedOn') IS NOT NULL`
+  );
+
+  console.log('Fixing old pics finish');
+
+  return Store.prototype.findAll.apply(modelStore);
+};
+
 export const locationsStore = new Store({
   storeName: 'locations',
   debugging: isDemo,
