4aaf400ae1caf1326498dbf13312adcc35b23f53
Karolis Kazlauskis <karolis@flumens.io>
2025-03-24 17:14:25 +0200

    Update About page

--- src/Info/About/index.tsx
+++ src/Info/About/index.tsx
@@ -1,69 +1,62 @@
-import { Trans as T } from 'react-i18next';
 import { Page, Main, Header, Section } from '@flumens';
-import appModel from 'models/app';
 import './styles.scss';
 
 const { P } = Section;
 
 const Component = () => {
-  const isEnglish = appModel.data.language === 'en';
-
   return (
     <Page id="about">
       <Header title="About" />
       <Main id="about" className="ion-padding">
-        {isEnglish ? (
-          <Section>
-            <P>
-              This app enables you to contribute to the recording and
-              conservation of Europe’s butterflies. The app is not part of the
-              Big Butterfly Count that is run in the UK by Butterfly
-              Conservation each summer. Please visit their{' '}
-              <a href="https://www.bigbutterflycount.org/">project website</a>{' '}
-              for information on how to get involved in that survey. Data
-              collected through this ButterflyCount app is shared with UK
-              Butterfly Conservation (and other Butterfly Conservation Europe
-              partners) to support their monitoring and conservation work.
-            </P>
-          </Section>
-        ) : (
-          <Section>
-            <P>
-              This app contributes data to the European Butterfly Monitoring
-              Scheme (eBMS).
-            </P>
-          </Section>
-        )}
-
         <Section>
           <P>
-            The app was developed as part of the Assessing Butterflies in Europe
-            (ABLE) project.
+            This app enables you to contribute to the recording and conservation
+            of butterflies across the world.
           </P>
         </Section>
-
         <Section>
+          <b>
+            <P>The app was supported through several projects:</P>
+          </b>
+
           <P skipTranslation>
-            <T>The ABLE project is a partnership between</T>: <br />
-            <a href="http://www.bc-europe.eu/">Butterfly Conservation Europe</a>
-            ,{' '}
-            <a href="www.ceh.ac.uk">
-              the Centre for Ecology and Hydrology (UK)
+            <ul className="ml-2 !list-disc">
+              <li>
+                Assessing Butterflies in Europe (ABLE) project, funded by the
+                European Union
+              </li>
+              <li>
+                Strengthening pollinator recovery through indicators and
+                monitoring (SPRING) project, funded by the European Union
+              </li>
+              <li>
+                Agroecology-TRANSECT and UGPplus. Helmholtz Centre for
+                Environmental Research (UFZ, Germany).
+              </li>
+              <li>
+                Expanding Monitoring of Butterflies for Restoration And
+                Conservation across Europe 2021-2026 (EMBRACE) project, funded
+                by the European Union
+              </li>
+
+              <li>
+                GLOBMS, funded as the part of the UK Centre for Ecology &
+                Hydrology (UKCEH) National Capability Internation programme,
+                funded by the Natural Environment Research Council (NERC)
+              </li>
+            </ul>
+          </P>
+        </Section>
+        <Section>
+          <P>
+            The app has benefited from input from a number of butterfly experts
+            from across the world and associated with national Butterfly
+            Monitoring Schemes listed at:
+          </P>
+          <P skipTranslation>
+            <a href="https://butterfly-monitoring.net/partners">
+              https://butterfly-monitoring.net/partners
             </a>
-            ,{' '}
-            <a href="https://www.ufz.de/index.php">
-              the Helmholtz Centre for Environmental Research (UFZ, Germany) -
-              Josef Settele, Oliver Schweiger
-            </a>
-            ,{' '}
-            <a href="https://www.vlinderstichting.nl/">
-              Dutch Butterfly Conservation
-            </a>{' '}
-            <T>and</T>{' '}
-            <a href="https://butterfly-conservation.org/">
-              Butterfly Conservation (UK)
-            </a>
-            .{' '}
           </P>
         </Section>
       </Main>
