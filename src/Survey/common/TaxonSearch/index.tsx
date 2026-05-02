a78741c466c3848325f067d61e87b8b506e50d7b
Karolis Kazlauskis <karolis@flumens.io>
2026-05-02 15:58:08 +0300

    Remove isPresent function from TaxonSearch component to simplify filtering logic

--- src/Survey/common/TaxonSearch/index.tsx
+++ src/Survey/common/TaxonSearch/index.tsx
@@ -108,30 +108,6 @@ const speciesGroupFilter = (
   ) as SQL<any>;
 };
 
-const isPresent = (table: typeof taxaStore.table): SQL => {
-  const { country } = appModel.data;
-  if (country === 'ELSEWHERE') return sql`1`;
-
-  // convert UK to GB for country code
-  let countryCode = country === 'UK' ? 'GB' : country;
-  if (!countryCode) return sql`1`;
-
-  const countryCodeWithSubregion = countryCode.includes('_');
-  if (countryCodeWithSubregion) {
-    countryCode = countryCode.replaceAll('_', ': '); // normalize it to match the data format
-  }
-
-  return or(
-    not(eq(table.taxon_group_id, groups.butterflies.id)), // abundance available only for butterflies group
-    inArray(sql`json_extract(${table.data}, '$.' || ${countryCode})`, [
-      'P',
-      'P?',
-      'M',
-      'I',
-    ])
-  ) as SQL<any>;
-};
-
 const taxonListFilter = (
   table: typeof taxaStore.table,
   taxonListCids?: string[],
@@ -189,7 +165,6 @@ const TaxonSearch = ({
         and(
           speciesGroupFilter(table, speciesGroups),
           taxonListFilter(table, taxonListCids, skipTaxonLists),
-          isPresent(table),
           filterDayFlyingMoths(table, useDayFlyingMothsOnly)
         ),
     });
