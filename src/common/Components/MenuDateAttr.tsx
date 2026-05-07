faf9fa7a1f9fb680f56cef5cc3fabf6f07242895
Karolis Kazlauskis <karolis@flumens.io>
2026-05-07 14:09:17 +0300

    Allow empty date time component values

--- src/common/Components/MenuDateAttr.tsx
+++ src/common/Components/MenuDateAttr.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { calendarOutline } from 'ionicons/icons';
 import { Trans as T } from 'react-i18next';
 import { DatetimePresentation } from '@ionic/core';
@@ -39,11 +40,24 @@ const MenuDateAttr = ({
 
     <div className="flex items-center gap-1">
       <div>
-        <IonDatetimeButton
-          datetime={`date-time-picker-${id || label}`}
-          slot="end"
-          className="[--ion-text-color:var(--form-value-color)]"
-        />
+        <div className={clsx('relative', !value && 'bg-[#edeef0] rounded-md')}>
+          {!value && (
+            <span className="absolute left-1/2 -translate-x-1/2 py-2 opacity-50 text-xs">
+              {presentation === 'time' ? '-- : --' : ''}
+            </span>
+          )}
+          <IonDatetimeButton
+            datetime={`date-time-picker-${id || label}`}
+            slot="end"
+            className={clsx(
+              '[--ion-text-color:var(--form-value-color)]',
+              !value && '[&::part(native)]:opacity-0'
+            )}
+            onClick={() => {
+              if (!value) onChange(new Date().toISOString());
+            }}
+          />
+        </div>
         <IonModal keepContentsMounted className="[--border-radius:10px]">
           <IonDatetime
             id={`date-time-picker-${id || label}`}
@@ -52,7 +66,7 @@ const MenuDateAttr = ({
             onIonChange={(e: any) => onChange(e.detail.value)}
             value={value}
             disabled={isDisabled}
-            max={max || new Date().toISOString()}
+            max={max}
             formatOptions={{
               date: { day: '2-digit', month: '2-digit', year: '2-digit' },
               time: { hour: '2-digit', minute: '2-digit' },
